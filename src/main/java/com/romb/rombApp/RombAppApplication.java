793683fe85d30be169ec6f6c7a86033d92cc257b
Huseyn Jafarli <hjeferli18088@ada.edu.az>
2025-04-15 20:22:29 +0400

    dependencies added, project started

---
+++ src/main/java/com/romb/rombApp/RombAppApplication.java
@@ -0,0 +1,13 @@
+package com.romb.rombApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RombAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RombAppApplication.class, args);
+	}
+
+}
