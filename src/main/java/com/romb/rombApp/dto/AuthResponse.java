5578aeded245e7856b57d01dd46a044eee3799ea
Huseyn Jafarli <hjeferli18088@ada.edu.az>
2025-05-22 01:00:51 +0400

    response dto fixed

--- src/main/java/com/romb/rombApp/dto/AuthResponse.java
+++ src/main/java/com/romb/rombApp/dto/AuthResponse.java
@@ -1,10 +1,14 @@
 package com.romb.rombApp.dto;
 
+import com.romb.rombApp.model.Role;
+
 import lombok.Data;
 
 @Data
 public class AuthResponse {
     private String token;
     private Long expiresIn;
-
+    private String username;
+    private String fullName;
+    private Role role;
 }
